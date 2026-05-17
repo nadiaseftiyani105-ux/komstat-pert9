972e9769224a62c2ed5c33017264556c27e6ae89
Nadia Seftiyani <nadiaseftiyani105@gmail.com>
2026-05-17 23:24:07 +0700

    Commit pertama tugas komstat

--- INI TUGAS KOMSTAT.R
+++ INI TUGAS KOMSTAT.R
@@ -3,5 +3,5 @@ mean(mtcars$mpg)   # Menghitung rata-rata (mean) mpg
 sd(mtcars$mpg)     # Menghitung standar deviasi (simpangan baku) mpg
 max(mtcars$mpg)    # Menghitung nilai tertinggi dari mpg
 min(mtcars$mpg)
-
+hist(mtcars$cyl)
 plot(mtcars$mpg)
