62067aa7006608c33ab03f487b02a202514f0db4
Nadia Seftiyani <nadiaseftiyani105@gmail.com>
2026-05-17 23:30:48 +0700

    "tambah plot"

--- INI TUGAS KOMSTAT.R
+++ INI TUGAS KOMSTAT.R
@@ -3,5 +3,4 @@ mean(mtcars$mpg)   # Menghitung rata-rata (mean) mpg
 sd(mtcars$mpg)     # Menghitung standar deviasi (simpangan baku) mpg
 max(mtcars$mpg)    # Menghitung nilai tertinggi dari mpg
 min(mtcars$mpg)
-
 plot(mtcars$mpg)
